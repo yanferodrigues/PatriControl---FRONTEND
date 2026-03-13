5485a471b75db4f4226a229996a041e206b3d80d
Yan felipe <yanfelipe678@gmail.com>
2026-03-13 17:00:25 -0300

    ESCRITÓRIO 13/03/2026 17:20

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,21 +1,74 @@
 const graficoDB = document.getElementById("grafico-dashboard")
 
-
 new Chart(graficoDB, {
-    type: 'pie',
+    type: 'doughnut',
+
     data: {
+        labels: [
+            'Veiculos',
+            'Equipamentos diversos',
+            'Maquinas',
+            'Informatica e comunicação',
+            'Móveis e utensílios',
+            'Imóveis'
+        ],
+
         datasets: [{
             label: 'Quantidade',
-            data: [200, 450, 600, 900,500],
-            borderColor: ['#1f2f47','#2d512f','#562c2c','#4e4421','#3f2a4c'],
-            backgroundColor:['#3569d9','#5fc266','#d94e4e','#efc83b','#af76d5'],
-        }],
-        labels: ['Veiculos', 'Equipamentos diversos', 'Maquinas', 'Informatica e comunicação', 'Móveis e utensílios']
+            data: [200, 450, 600, 900, 500,60],
+
+            borderColor: [
+                '#1f2f47',
+                '#2d512f',
+                '#562c2c',
+                '#4e4421',
+                '#3f2a4c'
+            ],
+
+            backgroundColor: [
+                '#3569d9',
+                '#5fc266',
+                '#d94e4e',
+                '#efc83b',
+                '#af76d5',
+                '#84f2cb'
+            ],
+
+            borderWidth: 2,
+            hoverOffset: 8
+        }]
     },
+
     options: {
-        cutout: '60%',
+        responsive: true,
+        maintainAspectRatio: false,
         animation: {
-            duration: 2500
+            animateRotate: true,
+            duration: 2000,
+            easing: 'easeInOutQuart' 
+        },
+        cutout: "65%",
+
+        plugins: {
+            legend: {
+                position: 'bottom',
+                align: 'start',
+
+                labels: {
+                    usePointStyle: true,
+                    pointStyle: 'circle',
+                    boxWidth: 10,
+                    padding: 15,
+                    color: "#ffffff"
+                }
+            }
+        },
+
+        layout: {
+            padding: {
+                top: 20,
+                bottom: 20
+            }
         }
     }
 });
