068e008e090e06b34c91050cb78247df3aaf2c0c
Yan Felipe Rodrigues <yanfelipe678@gmail.com>
2026-03-18 21:11:15 -0300

    CASA 18/03/2026 21:00

---
+++ assets/js/geral.js
@@ -0,0 +1,9 @@
+const overlay = document.querySelector(".overlay")
+const aside = document.getElementById("aside")
+
+function abrirMenu(btn) {
+    overlay.classList.toggle("ativo")
+    aside.classList.toggle("ativo")
+}
+
+
